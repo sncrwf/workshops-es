43a18b4f5522fd042948db8a14b61fed6183ac86
Lucas Almeida <lucas.b.almeida@live.com>
2024-12-06 17:12:00 -0300

    remove csm link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -275,10 +275,6 @@ const config = {
                 href: "https://developer.servicenow.com/dev.do#!/guides/vancouver/now-platform/glossary/developer-glossary",
               },
               {
-                label: "CSM-VA-Utah",
-                href: "/labs/csm-va-utah/overview",
-              },
-              {
                 label: "Now Create",
                 href: "https://nowlearning.servicenow.com/nowcreate",
               },
